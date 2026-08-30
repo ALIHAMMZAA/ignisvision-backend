30dcb4f03a67ff7b843c403cb96b1ed3bc4323cf
Ali Hamza <alihamza7org@gmail.com>
2026-08-30 21:44:12 +0530

    added vercel stable domain

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-       "https://ignisvision-frontend-lkgs52ino-tech-titans-5cec.vercel.app",
+       "https://ignisvision-frontend.vercel.app",
         
     ],
     allow_credentials=True,
